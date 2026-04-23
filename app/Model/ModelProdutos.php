ba5d2c82f1988588dc40f1b95c1237006eabf216
Igor Colaço <igor.colaco.bremer@gmail.com>
2026-04-23 09:25:45 -0300

    Update ModelProdutos

--- app/Model/ModelProdutos.php
+++ app/Model/ModelProdutos.php
@@ -19,6 +19,18 @@
             $stmt = $this->conn->prepare("INSERT INTO produtos (nome, codigo, preco, estoque, tipo) VALUES (?, ?, ?, ?, ?)");
             $stmt->bind_param("ssdii", $nome, $codigo, $preco, $estoque, $tipo);
             return $stmt->execute();
-
         }
+
+        public function editarProduto($nome, $preco, $estoque, $tipo, $codigo){
+            $stmt = $this->conn->prepare("UPDATE produtos SET nome = ?, preco = ?, estoque = ?, tipo = ? WHERE codigo = ?");
+            $stmt->bind_param("sdiii", $nome, $preco, $estoque, $tipo, $codigo);
+            return $stmt->execute();
+        }
+
+        public function excluirProduto($codigo){
+            $stmt = $this->conn->prepare("DELETE FROM produtos WHERE codigo = ?");
+            $stmt->bind_param("i", $codigo);
+            return $stmt->execute();
+        }
+
     }
